c5003d87a5a5e94b03822435519d07ba9c92e9cd
Will Ross <93093979+wross9959@users.noreply.github.com>
2025-03-24 21:39:53 -0300

    Update email_util.py

--- backend/email_util.py
+++ backend/email_util.py
@@ -7,16 +7,15 @@ import settings
 # function to call for sending emails from our UNB server acount
 def send_email(email_to, subject, body):
 
-    msg = MIMEText(body, 'html')
+    msg = MIMEText(body, 'plain')
     msg['Subject'] = subject
     msg['From'] = settings.UNB_SMTP_USER
     msg['To'] = email_to
 
     try:
-        server = smtplib.SMTP(settings.UNB_SMTP_HOST, settings.UNB_SMTP_PORT)
-        server.starttls()
-        server.sendmail(settings.UNB_SMTP_USER, email_to, msg.as_string())
-        server.quit()
+        s = smtplib.SMTP(settings.UNB_SMTP_HOST)
+        s.sendmail(settings.UNB_SMTP_USER, email_to, msg.as_string())
+        s.quit()
         return True
     except Exception as e:
         print(f"ERROR: Failed to send email: {e}")
